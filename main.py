60190948045d4021ff79c1658a6f3c7db802964c
andresxDDDD <moraga_2006+1@hotmail.com>
2026-04-12 03:37:42 +0000

    avance ejercicio final

---
+++ main.py
@@ -0,0 +1,66 @@
+from menu.menu import menu_principal,limpiar_pantalla
+""" from funciones.mostrar_catalogo import ver_productos, buscar_producto
+from productos.catalogo import catalogo
+from carrito.agregar_carrito import agrega_producto,calcular_total,mostrar_carrito,vaciar_carrito
+
+ """
+def main():
+    carr_prod =[]
+
+  
+limpiar_pantalla()
+opcion=menu_principal()
+""" 
+        if opcion ==1:
+      
+            limpiar_pantalla()
+            ver_productos(catalogo)
+            input("\nPresiona Enter para volver al menú...")
+
+        elif opcion ==2:
+
+            limpiar_pantalla()
+            print("------Bienvenido a nuestro buscador de productos:------")
+            msj=input("Ingrese Nombre o categoria del producto a buscar:")
+            buscar_producto(catalogo,msj)
+            input("\nPresiona Enter para volver al menú...")
+
+
+        elif opcion ==3:
+            limpiar_pantalla()
+            print("Agregar productos al carrito:")
+            op1= int(input("ingrese ID del producto a comprar:"))
+            op2= int(input("Ingrese la cantidad:"))
+            agrega_producto(catalogo,carr_prod,op1,op2)
+            input("\nPresiona Enter para volver al menú...")
+
+        elif opcion ==4:
+            limpiar_pantalla()
+            print("Ver carro de compras:")
+            
+            if not carr_prod:
+                print("El carrito está vacío.")
+            else:
+                mostrar_carrito(carr_prod)
+               
+            print("Total a pagar $",calcular_total(carr_prod))
+            
+
+            input("\nPresiona Enter para volver al menú...")
+
+
+        elif opcion ==5:
+            limpiar_pantalla()
+            print("------Vaciar el carro de compras------")
+            if not carr_prod:
+                print("El carrito está vacío.")
+            else:    
+             op3 = input("Escriba SI para vaciar carrito:")
+             vaciar_carrito(carr_prod,op3)
+            input("\nPresiona Enter para volver al menú...")
+
+
+        elif opcion ==0:
+           break
+ """
+main()    
---
+++ menu/menu.py
@@ -0,0 +1,68 @@
+import os
+
+def menu_principal():
+    while True:
+        limpiar_pantalla()
+        print("==========================================")
+        print("       SISTEMA DE ECOMMERCE ")
+        print("==========================================")
+        print("1. Ingresar como ADMINISTRADOR")
+        print("2. Ingresar como CLIENTE")
+        print("3. Salir")
+        print("------------------------------------------")
+        
+        rol = input("Seleccione su rol: ")
+    
+        if rol == "1":
+            menu_admin()
+        elif rol == "2":
+            menu_cliente()
+        elif rol == "3":
+            print("\n¡Gracias por usar la app! Saliendo...")
+            break
+        else:
+            print("\nOpción no válida. Intente de nuevo.")
+            input()
+
+def menu_cliente():
+    while True:
+        print("==========================================")
+        print("        PANEL DE CLIENTE")
+        print("==========================================")
+        print("1- Ver catalogos de productos:")
+        print("2- Buscar productos:")
+        print("3- Agregar productos al carro:")
+        print("4- Ver carro:")
+        print("5- Vaciar carro:")
+        print("0- Salir:")
+
+        opcion= int(input("Ingrese una opcion: "))
+        return opcion
+
+
+def menu_admin():
+    while True:
+        limpiar_pantalla()
+        print("==========================================")
+        print("        PANEL DE ADMINISTRACIÓN")
+        print("==========================================")
+        print("1. Ver inventario completo")
+        print("2. Agregar nuevo producto")
+        print("3. Actualizar stock/precio")
+        print("4. Eliminar producto")
+        print("5. <-- Volver al Menú Principal")
+        print("------------------------------------------")
+        
+        opcion = input("Seleccione una acción: ")
+        
+        if opcion == "5":
+            break
+        elif opcion in ["1", "2", "3", "4"]:
+            print(f"\nHas seleccionado la opción {opcion}. (Aquí irá la lógica)")
+            input("\nPresione Enter para continuar...")
+        else:
+            print("\nOpción no válida. Intente de nuevo.")
+            input()
+
+def limpiar_pantalla():
+    os.system('cls' if os.name == 'nt' else 'clear')
